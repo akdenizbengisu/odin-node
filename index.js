0218c904dcf89e9917d87c2608d07482c7f34fa4
tospili <bengisu343@hotmail.com>
2021-05-04 11:31:48 +0300

    Comment on express.static added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8080;
 const path = require('path')
 
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
